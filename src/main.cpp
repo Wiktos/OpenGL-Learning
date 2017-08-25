55552999499cb9f87587255242cb0540c86e85a9
Wiktor Łazarski <wlazarski@interia.pl>
2017-08-25 17:00:50 +0200

    Drawing rectangle algorithm using EBO

--- src/main.cpp
+++ src/main.cpp
@@ -83,27 +83,38 @@ int main()
 	glDeleteShader(fragmentShader);
 
 	GLfloat vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f,  0.5f, 0.0f
+		0.5f,  0.5f, 0.0f,  // top right
+		0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
+
+	GLuint indices[] = {  
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
 	};
 
 	GLuint VertexBufferObject;
 	GLuint VertexArrayObject;
+	GLuint ElementBufferObject;
 
 	glGenVertexArrays(1, &VertexArrayObject);
 	glGenBuffers(1, &VertexBufferObject);
+	glGenBuffers(1, &ElementBufferObject);
 
 	glBindVertexArray(VertexArrayObject);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VertexBufferObject);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ElementBufferObject);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
 	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);	
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	//render contexts
 	while (!appWindow.shouldWindowClose())
@@ -116,16 +127,18 @@ int main()
 	
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(0.1f, 0.5f, 0.8f, 1.0f);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VertexArrayObject);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, _countof(indices), GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 		glUseProgram(0);
 	}
 
-	glDeleteVertexArrays(1, &VertexArrayObject);
 	glDeleteBuffers(1, &VertexBufferObject);
+	glDeleteBuffers(1, &ElementBufferObject);
+	glDeleteVertexArrays(1, &VertexArrayObject);
 
 	GLFWAdapter::getInstance().terminate();
 	return 0;
