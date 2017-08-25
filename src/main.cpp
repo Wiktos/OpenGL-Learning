9559077245f9ffe6f8db8de2ebde156f1186757e
Wiktor Łazarski <wlazarski@interia.pl>
2017-08-25 18:08:51 +0200

    Sending data between shaders

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 #define GLEW_STATIC
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
 #include <iostream>
 
 #include "OpenGLWindow.h"
@@ -18,16 +16,19 @@ void simpleProcessInput(GLFWwindow* window)
 
 const GLchar* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"out vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"	vertexColor = vec4(1.0, 0.3, 0.8, 1.0);\n"
 "}\0";
 
 const GLchar* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = vertexColor;\n"
 "}\n\0";
 
 int main()
